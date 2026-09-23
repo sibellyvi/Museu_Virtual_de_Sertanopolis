13eafd552f3d9f6b5e7eeaceecc56ef0379e80f7
sibellyvih <sibellyvivi@gmail.com>
2026-09-23 19:56:46 -0300

    Adicionando conteúdo para a página principal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({  
+    site: 'https://sibellyvi.github.io',
+    base: '/Museu_Virtual_de_Sertanopolis',
+});
+
+
